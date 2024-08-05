20ac105951c25ce81bbd6e0c7b594ac77c7b6151
Kevin Luk <kevinluk07@gmail.com>
2024-08-05 20:47:22 +1000

    Revert "safari pref check fix"
    This reverts commit 15d105ac280e7b297f903ab36bd3960aa3bbe55e.

--- js/main.js
+++ js/main.js
@@ -263,7 +263,6 @@ function logEvent(event) {
     const { type, target } = event;
     const message = `${new Date().toLocaleTimeString()} - Event: ${type}, Target: ${target.tagName}`;
     console.log(message);
-    initialise();
 }
 
 // List of DOM events to listen for
